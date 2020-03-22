1882fe1fbb79e42ebd64d672b88102044d40db0d
tomquincey <tomquincey@gmail.com>
2020-03-22 20:31:08 +0000

    Day 2 part3

--- pabeta/script.js
+++ pabeta/script.js
@@ -266,7 +266,7 @@
   //var botcolour = "rgba(92, 0, 21, 0.4)"//"#32003e"
     
     var topcolour = "rgba(255, 189, 132, 0.8)" 
-    var botcolour = "rgba(245, 118, 142, 0.2)" 
+    var botcolour = "rgba(245, 118, 142, 0.3)" 
     
     lava0 = new LavaLamp(screen.width, screen.height, 7, topcolour, botcolour);
 
